1a7ca5b5ebb208e6cf950cb8f91776697f1f6a7b
VigneshwarJ <vigneshwarjayakumar@gmail.com>
2018-10-17 20:55:23 +0530

    hgvgy

--- Main.java
+++ Main.java
@@ -1,3 +1,38 @@
+<<<<<<< HEAD
+public class Main{
+    
+    public static void main(String[] args) {
+       
+        Display display = new Display(800,600,"hhh");// creates a new display
+        RenderContext target = display.GetFrameBuffer();//creates a render context
+        long previousTime = System.nanoTime();
+       
+        //StarField stars = new StarField(406, 64.0f, 30.0f); 
+        Vertex y_min = new Vertex(100.0f, 200.0f);
+        Vertex y_mid = new Vertex(50.0f,300.0f);
+        Vertex y_max = new Vertex(200.0f, 500.0f);
+        while (true) {
+            
+           // long currentTime = System.nanoTime();
+          //  int count=0;
+          //  float delta = (float)((currentTime - previousTime)/1000000000.0);
+            //stars.UpdateAndRender(target, delta);
+          
+           target.clear((byte)0x00);
+
+            target.DrawTriangle(y_min, y_mid, y_max);
+           // previousTime=currentTime;
+            // for (int i = 100; i < 200; i++) {
+            //     target.DrawScanBuffer(i,300-i,300+i);
+                
+            // }
+            
+           
+            display.swapBuffers();
+        }
+    }
+   
+=======
 public class Main {
 
     public static void main(String[] args) {
@@ -33,4 +68,5 @@ public class Main {
         }
     }
    
+>>>>>>> 89ea1d92999bc58d658f9b1f6e6f1ea853b96b88
 }
