b3798d5e0290a133a3c3f1e01a7c14649e50be9b
VigneshwarJ <vigneshwarjayakumar@gmail.com>
2018-10-17 21:03:30 +0530

    vikky

--- Main.java
+++ Main.java
@@ -10,25 +10,23 @@ public class Main {
         vertex ymid = new vertex(120, 100);
         vertex ymax = new vertex(150, 50);
         
-        //System.out.println("fool");
+       
         while (true) {
-            //System.out.println("fool1");
+            
             long currentTime = System.nanoTime();
             int count=0;
           //  float delta = (float)((currentTime - previousTime)/1000000000.0);
             //stars.UpdateAndRender(target, delta);
-           // System.out.println(delta);
+          
            target.clear((byte)0x00);
-           //System.out.println("fool");
+          
             previousTime=currentTime;
             target.drawTriangle(ymin,ymax,ymid);
         //     for (int i = 100; i < 200; i++) {
         //         target.DrawScanBuffer(i,300-i,300+i);
-        //         //System.out.println("inside loop");
+        //        
         //    }
            // target.fillShape(100,200);
-            //System.out.println(previousTime);
-            //System.out.println(delta);
             display.swapBuffers();
         }
     }
