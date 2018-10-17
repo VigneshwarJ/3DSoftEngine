b6f35be814f76fa7c4634caeedadc445ccf59088
VigneshwarJ <vigneshwarjayakumar@gmail.com>
2018-10-17 20:54:55 +0530

    staged

--- RenderContext.java
+++ RenderContext.java
@@ -22,70 +22,6 @@ public class RenderContext extends Bitmap {
         
 
     }
-<<<<<<< HEAD
-    public void DrawTriangle(Vertex y_min,Vertex y_mid,Vertex y_max)
-    {
-        Vertex temp;
-        float ymin = y_min.GetY();
-        float ymax = y_max.GetY();
-        float ymid = y_mid.GetY();
-        if(ymin>ymax)
-        {
-            temp = y_min;
-            y_min = y_max;
-            y_max = temp;
-        }
-        if(ymid>ymax)
-        {
-            temp = y_mid;
-            y_mid = y_max;
-            y_max = temp;
-
-        }
-        if(ymin>ymid)
-        {
-            temp = y_min;
-            y_min = y_mid;
-            y_mid = temp;
-
-        }
-        float xstep = (y_max.GetY() -y_min.GetY())/(y_max.GetX()-y_min.GetX());
-        float xstep1 = (y_max.GetY() -y_mid.GetY())/(y_max.GetX()-y_mid.GetX());
-        float xstep2 = (y_mid.GetY() -y_min.GetY())/(y_mid.GetX()-y_min.GetX());
-        float b = y_min.GetY() - (y_min.GetX() * xstep);
-            float b1 = y_mid.GetY() - (y_mid.GetX() * xstep1);
-            float b2 = y_min.GetY() - (y_min.GetX() * xstep2);
-        //System.out.println(b + "   " + y_min.GetY()+ "  "+(y_min.GetX()*xstep)+ ""+ y_min.GetX() );
-
-        for(int i =(int) (y_min.GetY());i<=(int)(y_max.GetY());i++)
-        {
-            float xmin = (((float)i - b)/(float)xstep);
-            float xMax =0;
-            if(i<=(int) (y_mid.GetY()))
-            {
-            xMax = (((float)i - b2)/(float)xstep2); 
-            }
-            else
-            xMax = (((float)i - b1)/(float)xstep1);
-            // float xMax = y_mid.GetX() + ((float)i/xstep2);
-            // System.out.println(xMax + " " +((float)i/xstep2));
-            //float xMax = y_max.GetX();
-        //     if(xMax>xmin)
-        //     {
-        //     System.out.println(xMax + "    "+ (((float)i - b)/(float)xstep)+"    " + xmin+"  " +i);
-        //     DrawScanBuffer(i, (int)xmin, (int)xMax);
-        // }
-        //     else{
-                System.out.println(xMax +"    " + xmin+"  " +i);
-                DrawScanBuffer(i, (int)xMax, (int)xmin);
-            // }
-           // DrawScanBuffer(i, (int)xMax, (int)xmin);
-
-        }
-        //System.out.println(y_max.GetY()+ "  "+y_min.GetY());
-        fillShape( (int)y_min.GetY(),  (int)y_max.GetY());
-        
-=======
     public void drawEdges(vertex y_min,vertex y_max,boolean whichside)
     {
         float xstep = (y_max.getY()-y_min.getY())/(y_max.getX() -y_min.getX());
@@ -138,7 +74,6 @@ public class RenderContext extends Bitmap {
         }
         
         fillShape((int)y_min.getY(), (int)y_max.getY());
->>>>>>> 89ea1d92999bc58d658f9b1f6e6f1ea853b96b88
     }
     public void fillShape(int yMin, int yMax)
     {
@@ -147,20 +82,12 @@ public class RenderContext extends Bitmap {
         {
             int xMin = m_scanBuffer[j*2];
             int xMax = m_scanBuffer[j*2 + 1];
-<<<<<<< HEAD
-            
-            for(int i=xMin;i<xMax;i++)
-            {
-                drawPixel(i, j, white, white, white, white);
-                
-=======
            // System.out.println(xMin);
           
             for(int i=xMin;i<xMax;i++)
             {
                 drawPixel(i, j, white, white, white, white);
                
->>>>>>> 89ea1d92999bc58d658f9b1f6e6f1ea853b96b88
             }
         }
     }
